e6d3f128e4d9da8590ae36bca5a2b160664fd7a9
Eoin Lynch <eplynch@protonmail.com>
2026-02-23 04:09:27 +0100

    Remove About route from app.routes.ts
    Removed the About route from the application routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Blog } from './pages/blog/blog';
 import { Projects } from './pages/projects/projects';
-import { About } from './pages/about/about';
+
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
   { path: 'blog', component: Blog },
   { path: 'projects', component: Projects },
-  { path: 'about', component: About },
   { path: '**', redirectTo: '' },
 ];
 
